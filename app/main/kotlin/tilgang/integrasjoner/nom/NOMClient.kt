c37bece880f040555a6cc1aac637415976fa0029
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-08-13 15:01:55 +0200

    mer logging på nom

--- app/main/kotlin/tilgang/integrasjoner/nom/NOMClient.kt
+++ app/main/kotlin/tilgang/integrasjoner/nom/NOMClient.kt
@@ -7,11 +7,13 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import org.slf4j.LoggerFactory
 import tilgang.LOGGER
 import tilgang.NOMConfig
 import tilgang.auth.AzureAdTokenProvider
 import tilgang.auth.AzureConfig
 import tilgang.http.HttpClientFactory
+import tilgang.integrasjoner.behandlingsflyt.BehandlingsflytClient
 import tilgang.metrics.cacheHit
 import tilgang.metrics.cacheMiss
 import tilgang.redis.Key
@@ -20,6 +22,8 @@ import tilgang.redis.Redis
 interface INOMClient {
     suspend fun personNummerTilNavIdent(søkerIdent: String): String
 }
+
+private val log = LoggerFactory.getLogger(BehandlingsflytClient::class.java)
 
 open class NOMClient(azureConfig: AzureConfig, private val redis: Redis, private val nomConfig: NOMConfig, private val prometheus: PrometheusMeterRegistry): INOMClient {
     val httpClient = HttpClientFactory.create()
@@ -31,10 +35,13 @@ open class NOMClient(azureConfig: AzureConfig, private val redis: Redis, private
     override suspend fun personNummerTilNavIdent(søkerIdent: String): String {
         val azureToken = azureTokenProvider.getClientCredentialToken()
 
+        log.info("Got token: $azureToken")
+
         if (redis.exists(Key("nom", søkerIdent))) {
             prometheus.cacheHit("nom").increment()
             return redis[Key("nom", søkerIdent)]!!.toNavident()
         }
+
         prometheus.cacheMiss("nom").increment()
 
         val query = NOMRequest.hentNavIdentFraPersonIdent(søkerIdent)
@@ -45,10 +52,14 @@ open class NOMClient(azureConfig: AzureConfig, private val redis: Redis, private
             setBody(query)
         }
 
+        log.info("Got response status: ${response.status}")
+
         return when (response.status) {
             HttpStatusCode.OK -> {
                 val result = response.body<NOMRespons>()
+                log.info("Got response body: $result")
                 val navIdentFraNOM = result.data?.ressurs?.navident.orEmpty()
+                log.info("Got navIdentFraNOM: $navIdentFraNOM")
                 redis.set(Key("nom", søkerIdent), navIdentFraNOM.toByteArray(), 3600)
                 navIdentFraNOM
             }
