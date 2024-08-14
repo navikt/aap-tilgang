9d4548e6ebd2971ab049a763448894a61748dc2f
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-08-14 11:12:58 +0200

    enable azure tokens igjen

--- app/test/kotlin/tilgang/regler/EgenSakRegelTest.kt
+++ app/test/kotlin/tilgang/regler/EgenSakRegelTest.kt
@@ -1,7 +1,20 @@
 package tilgang.regler
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import tilgang.Fakes
+import tilgang.NOMConfig
+import tilgang.auth.AzureConfig
+import tilgang.integrasjoner.nom.NOMClient
+import tilgang.integrasjoner.nom.toByteArray
+import tilgang.redis.Key
+import java.net.URI
 
 class EgenSakRegelTest {
     @Test
@@ -16,4 +29,44 @@ class EgenSakRegelTest {
         val navIdentFraNOM = "4321"
         assertTrue(EgenSakRegel.vurder(EgenSakInput(navAnsattIdent, navIdentFraNOM)))
     }
+
+    /*
+    @Test
+    fun `Kan serialisere og deserialisere`() {
+        Fakes().use{
+            val redis = it.redis
+            val azureConfig = AzureConfig(
+                clientId = "",
+                clientSecret = "",
+                tokenEndpoint = URI.create("http://localhost:1234").resolve("/token").toURL(),
+                jwks = URI.create("http://localhost:1234").resolve("/jwks").toURL(),
+                issuer = ""
+            )
+            val prometheus = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+            val nomClient = NOMClient(azureConfig, it.redis, NOMConfig("https://nom-api.intern.dev.nav.no/graphql", "api://dev-gcp.nom.nom-api/.default"), prometheus)
+
+            runBlocking {
+                nomClient.personNummerTilNavIdent("")
+
+                /*
+                redis.set(Key("nom", "me"), emptyRespons.toByteArray(), 3600)
+                if (redis.exists(Key("nom", "me"))) {
+                    val foundResp = redis[Key("nom", "me")]!!.toIdenterRespons()
+                    val x = 3
+                }*/
+            }
+        }
+
+    }
+
+    fun ByteArray.toNavident(): String {
+        val mapper = jacksonObjectMapper()
+        val tr = object : TypeReference<String>() {}
+        return mapper.readValue(this, tr)
+    }
+
+    fun String.toByteArray(): ByteArray {
+        val mapper = jacksonObjectMapper()
+        return mapper.writeValueAsBytes(this)
+    }*/
 }
