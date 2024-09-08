6f10e5c475c83c2dd807aa0e83a0c1f2a4212106
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2024-09-08 10:59:30 +0200

    Publiser kontrakt

--- api-kontrakt/build.gradle.kts
+++ api-kontrakt/build.gradle.kts
@@ -1,10 +1,39 @@
 plugins {
-    kotlin("jvm")
     id("aap-tilgang.conventions")
+    `maven-publish`
+    `java-library`
 }
 
 group = "no.nav.aap.tilgang"
 
 repositories {
     mavenCentral()
+}
+
+apply(plugin = "maven-publish")
+apply(plugin = "java-library")
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.name
+            version = project.findProperty("version")?.toString() ?: "0.0.0"
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/navikt/aap-tilgang")
+            credentials {
+                username = "x-access-token"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
