0faeaaeabe6c52779dd043b9747f5f982ea53029
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-01-15 06:55:12 +0100

    Gjør om BrukerIdentResolver til functional interface

--- plugin-kontrakt/src/main/kotlin/no/nav/aap/tilgang/plugin/kontrakt/BrukerIdentResolver.kt
+++ plugin-kontrakt/src/main/kotlin/no/nav/aap/tilgang/plugin/kontrakt/BrukerIdentResolver.kt
@@ -4,6 +4,6 @@ package no.nav.aap.tilgang.plugin.kontrakt
  * Brukes i forbindelse med auditlogging for å finne fnr for bruker det gjøres oppslag på.
  * For eksempel ved å slå opp ident i databasen vha. saksnummer
  */
-interface BrukerIdentResolver {
+fun interface BrukerIdentResolver {
     fun resolve(referanse: String): String
 }
