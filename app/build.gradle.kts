e08864a2408b1a066c7eeb7742bb0528dfbe6174
Nicolas N <nicolas.nordhagen@nav.no>
2025-04-01 21:45:05 +0200

    bump behandlingsflyt-kontrakt til 0.0.240

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 val ktorVersion = "3.1.2"
-val behandlingsflytVersjon = "0.0.238"
+val behandlingsflytVersjon = "0.0.240"
 val postmottakVersjon = "0.0.67"
 val komponenterVersjon = "1.0.193"
 
