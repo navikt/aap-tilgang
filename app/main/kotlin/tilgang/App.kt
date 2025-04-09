c79d2d83265e053e046228470bcb576a8f867b48
Nicolas N <nicolas.nordhagen@nav.no>
2025-04-09 10:47:25 +0200

    Fange timeout exception og redusere loglevel til warn

--- app/main/kotlin/tilgang/App.kt
+++ app/main/kotlin/tilgang/App.kt
@@ -2,23 +2,22 @@ package tilgang
 
 import com.papsign.ktor.openapigen.model.info.InfoModel
 import com.papsign.ktor.openapigen.route.apiRouting
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.auth.authenticate
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.routing
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import java.net.http.HttpTimeoutException
 import no.nav.aap.komponenter.server.AZURE
 import no.nav.aap.komponenter.server.commonKtorModule
-import tilgang.integrasjoner.saf.SafGraphqlClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import tilgang.service.EnhetService
-import tilgang.service.GeoService
 import tilgang.integrasjoner.behandlingsflyt.BehandlingsflytClient
 import tilgang.integrasjoner.behandlingsflyt.BehandlingsflytException
 import tilgang.integrasjoner.msgraph.MsGraphClient
@@ -28,6 +27,7 @@ import tilgang.integrasjoner.nom.NomException
 import tilgang.integrasjoner.pdl.PdlException
 import tilgang.integrasjoner.pdl.PdlGraphQLClient
 import tilgang.integrasjoner.saf.SafException
+import tilgang.integrasjoner.saf.SafGraphqlClient
 import tilgang.integrasjoner.skjerming.SkjermingClient
 import tilgang.integrasjoner.skjerming.SkjermingException
 import tilgang.metrics.uhåndtertExceptionTeller
@@ -36,6 +36,8 @@ import tilgang.regler.RegelService
 import tilgang.routes.actuator
 import tilgang.routes.tilgang
 import tilgang.service.AdressebeskyttelseService
+import tilgang.service.EnhetService
+import tilgang.service.GeoService
 import tilgang.service.SkjermingService
 
 val LOGGER: Logger = LoggerFactory.getLogger("aap-tilgang")
@@ -64,54 +66,65 @@ fun Application.api(
     val tilgangService = TilgangService(saf, behandlingsflyt, regelService)
 
     install(StatusPages) {
-        exception<PdlException> { call, cause ->
-            LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
-            prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i PDL: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
-        }
-        exception<MsGraphException> { call, cause ->
-            LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
-            prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i Microsoft Graph: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
-        }
-        exception<BehandlingsflytException> { call, cause ->
-            LOGGER.error(cause.message ?: "Uhåndtert feil ved kall til '${call.request.local.uri}'", cause)
-            prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i behandlingsflyt: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
-        }
-        exception<SafException> { call, cause ->
-            LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
-            prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i SAF: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
-        }
-        exception<NomException> { call, cause ->
-            LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
-            prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i NOM: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
-        }
-        exception<SkjermingException> { call, cause ->
-            LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
-            prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i skjerming: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
-        }
         exception<Throwable> { call, cause ->
-            LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
             prometheus.uhåndtertExceptionTeller(cause.javaClass.name).increment()
-            call.respondText(
-                text = "Feil i tjeneste: ${cause.message}", status = HttpStatusCode.InternalServerError
-            )
+
+            when (cause) {
+                is HttpTimeoutException -> {
+                    LOGGER.warn("Timeout mot '{}'", call.request.local.uri, cause)
+                    call.respondText("Timeout mot: '{}'", status = HttpStatusCode.RequestTimeout)
+                }
+
+                is PdlException -> {
+                    LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
+                    call.respondText(
+                        text = "Feil i PDL: ${cause.message}", status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+                is MsGraphException -> {
+                    LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
+                    call.respondText(
+                        text = "Feil i Microsoft Graph: ${cause.message}", status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+                is BehandlingsflytException -> {
+                    LOGGER.error(cause.message ?: "Uhåndtert feil ved kall til '${call.request.local.uri}'", cause)
+                    call.respondText(
+                        text = "Feil i behandlingsflyt: ${cause.message}", status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+                is SafException -> {
+                    LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
+                    call.respondText(
+                        text = "Feil i SAF: ${cause.message}", status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+                is NomException -> {
+                    LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
+                    call.respondText(
+                        text = "Feil i NOM: ${cause.message}", status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+                is SkjermingException -> {
+                    LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
+                    call.respondText(
+                        text = "Feil i skjerming: ${cause.message}", status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+                else -> {
+                    LOGGER.error("Uhåndtert feil ved kall til '{}'", call.request.local.uri, cause)
+                    call.respondText(
+                        text = "Feil i tjeneste: ${cause.message}",
+                        status = HttpStatusCode.InternalServerError
+                    )
+                }
+            }
         }
     }
 
