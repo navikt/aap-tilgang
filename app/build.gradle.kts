c3011d7927475042813f29c47ce1a2773e8e5b8d
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-05-19 10:27:10 +0200

    Bump behandlingsflytversjon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 val ktorVersion = "3.1.3"
-val behandlingsflytVersjon = "0.0.302"
+val behandlingsflytVersjon = "0.0.305"
 val postmottakVersjon = "0.0.92"
 val komponenterVersjon = "1.0.247"
 
