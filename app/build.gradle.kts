88304d0114b189183d225ed4a7962d30ebd8f693
Henrik Gundersen <henrikgun@gmail.com>
2025-08-08 12:33:53 +0200

    Aap 1179 oppdaterer versjon behandlingsflyt (#268)
    * AAP-1179 Ny stegtype lagt til i kontrakt
    
    * AAP-1179 Oppdaterer versjon behandlingsflyt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 val ktorVersion = "3.2.3"
-val behandlingsflytVersjon = "0.0.398"
+val behandlingsflytVersjon = "0.0.403"
 val postmottakVersjon = "0.0.114"
 val komponenterVersjon = "1.0.308"
 val mockOAuth2ServerVersion = "2.2.1"
