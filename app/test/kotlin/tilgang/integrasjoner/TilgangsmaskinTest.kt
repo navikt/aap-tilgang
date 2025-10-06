17a6fc8cc50b8f0576195b98f1690be4968fc62d
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-10-06 11:40:05 +0200

    fiks ny parsetest

--- app/test/kotlin/tilgang/integrasjoner/TilgangsmaskinTest.kt
+++ app/test/kotlin/tilgang/integrasjoner/TilgangsmaskinTest.kt
@@ -6,17 +6,18 @@ import tilgang.AzureTokenGen
 import tilgang.fakes.WithFakes
 import tilgang.integrasjoner.tilgangsmaskin.TilgangsmaskinAvvistGrunn
 import tilgang.integrasjoner.tilgangsmaskin.TilgangsmaskinClient
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class TilgangsmaskinTest : WithFakes {
     @Test
     fun `Kan parse harTilgangTilPersonKjerne`() {
         val token = AzureTokenGen("tilgangazure", "tilgang").generate()
-        val harTilgang = TilgangsmaskinClient().harTilgangTilPersonKjerne("123", OidcToken(token))
-        val harIkkeTilgang = TilgangsmaskinClient().harTilgangTilPersonKjerne("456", OidcToken(token))
+        val harTilgangResponse = TilgangsmaskinClient().harTilgangTilPersonKjerne("123", OidcToken(token))
+        val harIkkeTilgangResponse = TilgangsmaskinClient().harTilgangTilPersonKjerne("456", OidcToken(token))
 
-        assertTrue(harTilgang.harTilgang)
-        assertTrue(harIkkeTilgang.harTilgang)
-        assertTrue { harTilgang.TilgangsmaskinAvvistResponse?.title == TilgangsmaskinAvvistGrunn.AVVIST_HABILITET.toString() }
+        assertTrue(harTilgangResponse.harTilgang)
+        assertFalse(harIkkeTilgangResponse.harTilgang)
+        assertTrue(harIkkeTilgangResponse.TilgangsmaskinAvvistResponse?.title == TilgangsmaskinAvvistGrunn.AVVIST_HABILITET.toString())
     }
 }
