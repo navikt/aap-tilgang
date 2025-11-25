6d230e2368d19aa7d1061091a1c911fea9307769
Per Henden <per.christian.henden@nav.no>
2025-11-25 14:08:40 +0100

    Øk antall tråder ktor bruker til innkommende http connections.
    Angi shutdown timeouts.

--- app/main/kotlin/tilgang/App.kt
+++ app/main/kotlin/tilgang/App.kt
@@ -4,8 +4,13 @@ import com.papsign.ktor.openapigen.model.info.InfoModel
 import com.papsign.ktor.openapigen.route.apiRouting
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStopPreparing
+import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.application.ApplicationStopping
 import io.ktor.server.application.install
+import io.ktor.server.application.log
 import io.ktor.server.auth.authenticate
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.statuspages.StatusPages
@@ -18,6 +23,7 @@ import no.nav.aap.komponenter.server.AZURE
 import no.nav.aap.komponenter.server.commonKtorModule
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import tilgang.AppConfig.LOGGER
 import tilgang.integrasjoner.behandlingsflyt.BehandlingsflytGateway
 import tilgang.integrasjoner.behandlingsflyt.BehandlingsflytException
 import tilgang.integrasjoner.msgraph.MsGraphGateway
@@ -39,15 +45,47 @@ import tilgang.service.AdressebeskyttelseService
 import tilgang.service.EnhetService
 import tilgang.service.GeoService
 import tilgang.service.SkjermingService
+import kotlin.time.Duration.Companion.seconds
 
-val LOGGER: Logger = LoggerFactory.getLogger("aap-tilgang")
 
+internal object AppConfig {
+    val LOGGER: Logger = LoggerFactory.getLogger("aap-tilgang")
+
+    // Matcher terminationGracePeriodSeconds for podden i Kubernetes-manifestet ("nais.yaml")
+    private val kubernetesTimeout = 20.seconds
+
+    // Tid før ktor avslutter uansett. Må være litt mindre enn `kubernetesTimeout`.
+    val shutdownTimeout = kubernetesTimeout - 2.seconds
+
+    // Tid appen får til å fullføre påbegynte requests, jobber etc. Må være mindre enn `endeligShutdownTimeout`.
+    val shutdownGracePeriod = shutdownTimeout - 3.seconds
+
+    // Vi skrur opp ktor sin default-verdi, som er "antall CPUer", satt ved -XX:ActiveProcessorCount i Dockerfile,
+    // fordi appen vår er veldig I/O-bound når den venter på svar fra andre tjenester.
+    private val ktorParallellitet = 4
+    // Vi følger ktor sin metodikk for å regne ut tuning parametre som funksjon av parallellitet
+    // https://github.com/ktorio/ktor/blob/3.3.1/ktor-server/ktor-server-core/common/src/io/ktor/server/engine/ApplicationEngine.kt#L30
+    val connectionGroupSize = ktorParallellitet / 2 + 1
+    val workerGroupSize = ktorParallellitet / 2 + 1
+    val callGroupSize = ktorParallellitet
+
+}
 fun main() {
     Thread.currentThread().setUncaughtExceptionHandler { _, e -> LOGGER.error("Uhåndtert feil", e) }
 
     embeddedServer(
         Netty,
-        port = System.getenv("PORT")?.toInt() ?: 8080,
+        configure = {
+            connectionGroupSize = AppConfig.connectionGroupSize
+            workerGroupSize = AppConfig.workerGroupSize
+            callGroupSize = AppConfig.callGroupSize
+
+            shutdownGracePeriod = AppConfig.shutdownGracePeriod.inWholeMilliseconds
+            shutdownTimeout = AppConfig.shutdownTimeout.inWholeMilliseconds
+            connector {
+                port = System.getenv("PORT")?.toInt() ?: 8080
+            }
+        },
         module = Application::api
     ).start(wait = true)
 }
@@ -145,5 +183,16 @@ fun Application.api(
                 tilgang(tilgangService, config.roles, prometheus)
             }
         }
+    }
+
+
+    monitor.subscribe(ApplicationStopPreparing) { environment ->
+        environment.log.info("ktor forbereder seg på å stoppe.")
+    }
+    monitor.subscribe(ApplicationStopping) { environment ->
+        environment.log.info("ktor stopper nå å ta imot nye requester, og lar mottatte requester kjøre frem til timeout.")
+    }
+    monitor.subscribe(ApplicationStopped) { environment ->
+        environment.log.info("ktor har fullført nedstoppingen sin. Eventuelle requester og annet arbeid som ikke ble fullført innen timeout ble avbrutt.")
     }
 }
