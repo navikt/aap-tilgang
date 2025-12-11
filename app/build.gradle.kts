dc0d916692ed70da31432ab54ea8dd3ee2972b0a
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-12-11 08:13:03 +0100

    bump behandlingsflytVersjon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 val ktorVersion = "3.3.3"
-val behandlingsflytVersjon = "0.0.501"
+val behandlingsflytVersjon = "0.0.506"
 val postmottakVersjon = "6.0.2"
 val komponenterVersjon = "1.0.456"
 val mockOAuth2ServerVersion = "3.0.1"
