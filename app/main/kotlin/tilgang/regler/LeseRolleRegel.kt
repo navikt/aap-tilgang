15b5675c7b656af9c7f34c5859ac499303941d7e
Nicolas N. <nicolas.nordhagen@nav.no>
2026-05-05 09:52:24 +0200

    skru på texas igjen + trekke ut val til klassenivå

--- app/main/kotlin/tilgang/regler/LeseRolleRegel.kt
+++ app/main/kotlin/tilgang/regler/LeseRolleRegel.kt
@@ -3,13 +3,16 @@ package tilgang.regler
 import no.nav.aap.tilgang.Rolle
 
 data object LeseRolleRegel : Regel<List<Rolle>> {
+    private val tillatteRoller = setOf(
+        Rolle.SAKSBEHANDLER_OPPFOLGING,
+        Rolle.SAKSBEHANDLER_NASJONAL,
+        Rolle.BESLUTTER,
+        Rolle.LES,
+        Rolle.KVALITETSSIKRER
+    )
+
     override fun vurder(input: List<Rolle>): Boolean {
-        return input.any {
-            it in listOf(
-                Rolle.SAKSBEHANDLER_OPPFOLGING,
-                Rolle.SAKSBEHANDLER_NASJONAL, Rolle.BESLUTTER, Rolle.LES, Rolle.KVALITETSSIKRER
-            )
-        }
+        return input.any { it in tillatteRoller }
     }
 }
 
