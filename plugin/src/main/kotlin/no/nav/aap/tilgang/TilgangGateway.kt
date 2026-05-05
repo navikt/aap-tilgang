3a1649f56be125e63a37e77ded34bb66e7f7a27a
charliemidtlyng <charlie.midtlyng@nav.no>
2026-05-05 10:21:30 +0200

    Skal ha caching i tilgang-plugin-modulen for å slippe å gjøre http-kall hver gang behandlingsflyt/oppgave/postmottak gjør de samme sjekkene om og om igjen.

--- plugin/src/main/kotlin/no/nav/aap/tilgang/TilgangGateway.kt
+++ plugin/src/main/kotlin/no/nav/aap/tilgang/TilgangGateway.kt
@@ -1,6 +1,11 @@
 package no.nav.aap.tilgang
 
+import com.github.benmanes.caffeine.cache.Caffeine
+import io.micrometer.core.instrument.binder.cache.CaffeineCacheMetrics
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import java.net.URI
+import java.time.Duration
 import no.nav.aap.komponenter.config.requiredConfigForKey
 import no.nav.aap.komponenter.httpklient.httpclient.ClientConfig
 import no.nav.aap.komponenter.httpklient.httpclient.RestClient
@@ -12,6 +17,24 @@ import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.azurecc.OnBeha
 object TilgangGateway {
     private val baseUrl = URI.create(requiredConfigForKey("integrasjon.tilgang.url"))
     private val config = ClientConfig(scope = requiredConfigForKey("integrasjon.tilgang.scope"))
+    val prometheus = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    private val tilgangGatewayBehandlingCache = Caffeine.newBuilder()
+        .maximumSize(2_000)
+        .expireAfterWrite(Duration.ofMinutes(30))
+        .recordStats()
+        .build<BehandlingTilgangRequestMedToken, TilgangResponse>()
+
+    private val tilgangGatewaySakCache = Caffeine.newBuilder()
+        .maximumSize(2_000)
+        .expireAfterWrite(Duration.ofMinutes(30))
+        .recordStats()
+        .build<SakTilgangRequestMedToken, TilgangResponse>()
+
+    init {
+        CaffeineCacheMetrics.monitor(prometheus, tilgangGatewayBehandlingCache, "tilgang_behandling_cache")
+        CaffeineCacheMetrics.monitor(prometheus, tilgangGatewaySakCache, "tilgang_sak_cache")
+    }
 
     private val client = RestClient.withDefaultResponseHandler(
         config = config,
@@ -19,31 +42,35 @@ object TilgangGateway {
     )
 
     fun harTilgangTilSak(body: SakTilgangRequest, currentToken: OidcToken): TilgangResponse {
-        val httpRequest = PostRequest(
-            body = body,
-            currentToken = currentToken
-        )
-        val respons = requireNotNull(
-            client.retryablePost<_, TilgangResponse>(
-                uri = baseUrl.resolve("/tilgang/sak"),
-                request = httpRequest
+        return tilgangGatewaySakCache.get(SakTilgangRequestMedToken(body, currentToken)) {
+            val httpRequest = PostRequest(
+                body = body,
+                currentToken = currentToken
             )
-        )
-        return respons
+            val respons = requireNotNull(
+                client.retryablePost<_, TilgangResponse>(
+                    uri = baseUrl.resolve("/tilgang/sak"),
+                    request = httpRequest
+                )
+            )
+            respons
+        }
     }
 
     fun harTilgangTilBehandling(body: BehandlingTilgangRequest, currentToken: OidcToken): TilgangResponse {
-        val httpRequest = PostRequest(
-            body = body,
-            currentToken = currentToken
-        )
-        val respons = requireNotNull(
-            client.retryablePost<_, TilgangResponse>(
-                uri = baseUrl.resolve("/tilgang/behandling"),
-                request = httpRequest
+        return tilgangGatewayBehandlingCache.get(BehandlingTilgangRequestMedToken(body, currentToken)) {
+            val httpRequest = PostRequest(
+                body = body,
+                currentToken = currentToken
             )
-        )
-        return respons
+            val respons = requireNotNull(
+                client.retryablePost<_, TilgangResponse>(
+                    uri = baseUrl.resolve("/tilgang/behandling"),
+                    request = httpRequest
+                )
+            )
+            respons
+        }
     }
 
     fun harTilgangTilJournalpost(
@@ -91,3 +118,13 @@ object TilgangGateway {
         return respons
     }
 }
+
+private data class BehandlingTilgangRequestMedToken(
+    val behandlingTilgangRequest: BehandlingTilgangRequest,
+    val token: OidcToken
+)
+
+private data class SakTilgangRequestMedToken(
+    val sakTilgangRequest: SakTilgangRequest,
+    val token: OidcToken
+)
