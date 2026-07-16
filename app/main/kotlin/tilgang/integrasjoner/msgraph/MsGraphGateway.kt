0131ad9534a088873d3d35d03177cc9a64fe5cae
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-16 11:20:52 +0200

    Catch timeout-exception slik at det blir riktig respons

--- app/main/kotlin/tilgang/integrasjoner/msgraph/MsGraphGateway.kt
+++ app/main/kotlin/tilgang/integrasjoner/msgraph/MsGraphGateway.kt
@@ -3,6 +3,7 @@ package tilgang.integrasjoner.msgraph
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.HttpRequestTimeoutException
 import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
@@ -42,6 +43,8 @@ class MsGraphGateway(
             httpClient.get("$baseUrl/me/memberOf?\$top=500&\$select=id,mailNickname") {
                 bearerAuth(tokenProvider.oboToken(scope, currentToken))
             }.body<MemberOf>()
+        } catch (e: HttpRequestTimeoutException) {
+            throw e
         } catch (e: Exception) {
             throw MsGraphException(e.message ?: "Ukjent feil mot msgraph")
         }
