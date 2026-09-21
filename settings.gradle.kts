942b054d86d35b49b987f8ed4f5ad2a50089861a
Fredrik Meyer <fredrik.meyer@nav.no>
2026-09-21 08:38:38 +0200

    AAP-BAU Felles versjonkatalog (#684)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,11 @@ include("app", "plugin", "api-kontrakt", "plugin-kontrakt")
 
 dependencyResolutionManagement {
     // Felles for alle gradle prosjekter i repoet
+    versionCatalogs {
+        create("kelvinLibs") {
+            from("no.nav.aap.kelvin:version-catalog:2.0.163")
+        }
+    }
     @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
